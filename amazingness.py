9bdc5305c9a78fe03ee77f39ef82ee68e266941a
yuiiiiiii <55540287+yuiiiiiii@users.noreply.github.com>
2019-10-07 11:41:07 +0100

    new fiel pls ignore >~<

---
+++ amazingness.py
@@ -0,0 +1,35 @@
+import time, random
+
+## Shouldn't need to def too many variables this time >~<
+onii_chan = "amazing!"
+
+## Let's start some awesomeness :3
+def amazingness():
+  if onii_chan == "amazing!": ## should always be truee <3
+    yayy()
+  else:
+    aww()
+    
+## Should hopefully always goto this func <3    
+def yayy():
+  print("yayyy!!! Love you onii-channnnn~~!")
+  time.sleep(5)
+  print(":wuv::wuv::xfiakkohug::xfiakkohug:")
+  time.sleep(5)
+  end()
+  
+## Not sure why I'm even including this >~<  
+def aww():
+  print("Aww.... :meowsad:")
+  time.sleep(5)
+  end()
+
+## Nicer exit imo >~<
+def end():
+  print("Aww.. Guess it's time to go..")
+  time.sleep(1)
+  print("Baiii~~")
+  time.sleep(5)
+  exit()
+  
+## ~~ Yui Tomori 2019 ~~
